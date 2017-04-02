1c4438b43a3b6de86f8b3b9c56f30055f164df9e
ernestoherrera <ernesto.herrerajr@outlook.com>
2017-04-02 01:09:57 -0400

    Adding the non-nullable date to the SqlFunctions

--- FluentSql/SqlFunctions.cs
+++ FluentSql/SqlFunctions.cs
@@ -11,119 +11,424 @@ namespace FluentSql
         private static string FUNCTION_DIRECT_CALL = "This function can not be called directly.";
 
         #region DateAdd Function
+        /// <summary>
+        /// AddYears Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfYears"></param>
+        /// <returns></returns>
         public static DateTime AddYears(DateTime fieldName, int numberOfYears)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddYears Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfYears"></param>
+        /// <returns></returns>
         public static DateTime AddYears(DateTime? fieldName, int numberOfYears)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// Add Quarters Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfQuarters"></param>
+        /// <returns></returns>
+        public static DateTime AddQuarters(DateTime fieldName, int numberOfQuarters)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddQuarters Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfQuarters"></param>
+        /// <returns></returns>
         public static DateTime AddQuarters(DateTime? fieldName, int numberOfQuarters)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddMonths Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfMonths"></param>
+        /// <returns></returns>
+        public static DateTime AddMonths(DateTime fieldName, int numberOfMonths)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddMonths Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfMonths"></param>
+        /// <returns></returns>
         public static DateTime AddMonths(DateTime? fieldName, int numberOfMonths)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddDayOfYear Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfYears"></param>
+        /// <returns></returns>
         public static DateTime AddDayOfYear(DateTime? fieldName, int numberOfYears)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddDayOfYear Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfYears"></param>
+        /// <returns></returns>
+        public static DateTime AddDayOfYear(DateTime fieldName, int numberOfYears)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddDays Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfDays"></param>
+        /// <returns></returns>
         public static DateTime AddDays(DateTime? fieldName, int numberOfDays)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddDays Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfDays"></param>
+        /// <returns></returns>
+        public static DateTime AddDays(DateTime fieldName, int numberOfDays)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddWeeks Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfWeeks"></param>
+        /// <returns></returns>
         public static DateTime AddWeeks(DateTime? fieldName, int numberOfWeeks)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// Add Weeks Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfWeeks"></param>
+        /// <returns></returns>
+        public static DateTime AddWeeks(DateTime fieldName, int numberOfWeeks)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddDayOfWeek Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="dayOfWeek"></param>
+        /// <returns></returns>
         public static DateTime AddDayOfWeek(DateTime? fieldName, int dayOfWeek)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddDayOfWeek Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="dayOfWeek"></param>
+        /// <returns></returns>
+        public static DateTime AddDayOfWeek(DateTime fieldName, int dayOfWeek)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddHours Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfHours"></param>
+        /// <returns></returns>
         public static DateTime AddHours(DateTime? fieldName, int numberOfHours)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddHours Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfHours"></param>
+        /// <returns></returns>
+        public static DateTime AddHours(DateTime fieldName, int numberOfHours)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddMinutes Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfMinutes"></param>
+        /// <returns></returns>
         public static DateTime AddMinutes(DateTime? fieldName, int numberOfMinutes)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddMinutes Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfMinutes"></param>
+        /// <returns></returns>
+        public static DateTime AddMinutes(DateTime fieldName, int numberOfMinutes)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddSeconds Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfSeconds"></param>
+        /// <returns></returns>
         public static DateTime AddSeconds(DateTime? fieldName, int numberOfSeconds)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// AddSeconds Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfSeconds"></param>
+        /// <returns></returns>
+        public static DateTime AddSeconds(DateTime fieldName, int numberOfSeconds)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddMilliseconds Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfMilliseconds"></param>
+        /// <returns></returns>
         public static DateTime AddMilliseconds(DateTime? fieldName, int numberOfMilliseconds)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// AddMilliseconds Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="numberOfMilliseconds"></param>
+        /// <returns></returns>
+        public static DateTime AddMilliseconds(DateTime fieldName, int numberOfMilliseconds)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
         #endregion
 
         #region DatePart Function
+        /// <summary>
+        /// GetYear Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetYear(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// GetYear Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetYear(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// GetQuarter Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetQuarter(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// GetQuarter Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetQuarter(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// GetMonth Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetMonth(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// GetMonth Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetMonth(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// GetDayOfYear Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetDayOfYear(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// GetDayOfYear Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetDayOfYear(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// GetDay Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetDay(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// GetDay Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetDay(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// GetWeek Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetWeek(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// GetWeek Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetWeek(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// GetWeekDay Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetWeekday(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// GetWeekDay Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetWeekday(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// GetHour Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetHour(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// GetHour Method
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetHour(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetMinute(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetMinute(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetSecond(DateTime? fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetSecond(DateTime fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
         public static int GetMillisecond(DateTime? fieldName)
+        {
+            throw new NotSupportedException(FUNCTION_DIRECT_CALL);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        public static int GetMillisecond(DateTime fieldName)
         {
             throw new NotSupportedException(FUNCTION_DIRECT_CALL);
         }
